95ad4f8671889e6e243ef46d93d96e012a6a9505
Phan Hieu <hieutrunglephan@gmail.com>
2018-08-02 11:06:31 +0700

    test middleware
    - add test for midware delete

--- service/category/mw_validation_test.go
+++ service/category/mw_validation_test.go
@@ -137,3 +137,40 @@ func Test_validationMiddleware_Update(t *testing.T) {
 		})
 	}
 }
+
+func Test_validationMiddleware_Delete(t *testing.T) {
+	fakeID := domain.MustGetUUIDFromString("1698bbd6-e0c8-4957-a5a9-8c536970994b")
+
+	serviceMock := &ServiceMock{
+		DeleteFunc: func(_ context.Context, p *domain.Category) error {
+			return nil
+		},
+	}
+
+	type args struct {
+		category *domain.Category
+	}
+	tests := []struct {
+		name            string
+		args            args
+		wantErr         bool
+		errorStatusCode int
+	}{
+		{
+			name: "success",
+			args: args{&domain.Category{
+				Model: domain.Model{ID: fakeID},
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mw := validationMiddleware{
+				Service: serviceMock,
+			}
+			if err := mw.Delete(context.Background(), tt.args.category); (err != nil) != tt.wantErr {
+				t.Errorf("validationMiddleware.Create() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
