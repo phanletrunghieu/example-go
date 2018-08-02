460322129cdc293060c68765e15b2f26f78e291e
Phan Hieu <hieutrunglephan@gmail.com>
2018-08-02 11:11:44 +0700

    update mw test
    - test for mw delete

--- service/book/mw_validation_test.go
+++ service/book/mw_validation_test.go
@@ -179,3 +179,40 @@ func Test_validationMiddleware_Update(t *testing.T) {
 		})
 	}
 }
+
+func Test_validationMiddleware_Delete(t *testing.T) {
+	fakeID := domain.MustGetUUIDFromString("1698bbd6-e0c8-4957-a5a9-8c536970994b")
+
+	serviceMock := &ServiceMock{
+		DeleteFunc: func(_ context.Context, p *domain.Book) error {
+			return nil
+		},
+	}
+
+	type args struct {
+		book *domain.Book
+	}
+	tests := []struct {
+		name            string
+		args            args
+		wantErr         bool
+		errorStatusCode int
+	}{
+		{
+			name: "success",
+			args: args{&domain.Book{
+				Model: domain.Model{ID: fakeID},
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mw := validationMiddleware{
+				Service: serviceMock,
+			}
+			if err := mw.Delete(context.Background(), tt.args.book); (err != nil) != tt.wantErr {
+				t.Errorf("validationMiddleware.Create() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
